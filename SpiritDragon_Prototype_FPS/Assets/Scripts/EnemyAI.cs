c463e1307be7699785f00bd7736928bcdcf74f32
ThomasHowse <TCHowse@student.fullsail.edu>
2023-02-04 13:40:57 -0500

    Pushing what i got

--- SpiritDragon_Prototype_FPS/Assets/Scripts/EnemyAI.cs
+++ SpiritDragon_Prototype_FPS/Assets/Scripts/EnemyAI.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -97,3 +98,105 @@ public class EnemyAI : MonoBehaviour, IDamage
         }
     }
 }
+=======
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour, IDamage
+{
+    [Header("----- Components -----")]
+    [SerializeField] Renderer model;
+    [SerializeField] NavMeshAgent agent;
+
+    [Header("----- Stats -----")]
+    [Range(1,10)][SerializeField] int Hp;
+    [SerializeField] int playerFaceSpeed;
+
+    [Header("----- Gun -----")]
+    [SerializeField] Transform shootPos;
+    [SerializeField] GameObject bullet;
+    [SerializeField] int bulletSpeed;
+    [SerializeField] float fireRate;
+
+    Vector3 plyrDir;
+    bool Shooting, NRange;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       //gameManager.instance.updateGameGoal(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        plyrDir = gameManager.instance.player.transform.position - transform.position;
+        if(NRange)
+        {
+            agent.SetDestination(gameManager.instance.player.transform.position);
+
+            if(agent.remainingDistance < agent.stoppingDistance)
+            {
+                facePlayer();
+            }
+            if (!Shooting)
+                StartCoroutine(shoot());
+        }
+    }
+
+    public void takeDamage(int dmg)
+    {
+        Hp -= dmg;
+        StartCoroutine(flshDmg());
+
+        if(Hp <= 0)
+        {
+            gameManager.instance.updateGameGoal(-1);
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator flshDmg()
+    {
+        Color basic = model.material.color;
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(.2f);
+        model.material.color = basic;
+    }
+
+    void facePlayer()
+    {
+        Quaternion rot = Quaternion.LookRotation(plyrDir);
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * playerFaceSpeed);
+    }
+
+    IEnumerator shoot()
+    {
+        Shooting = true;
+        GameObject clone = Instantiate(bullet, shootPos.position, bullet.transform.rotation);
+        clone.GetComponent<Rigidbody>().velocity = transform.forward * bulletSpeed;
+
+        yield return new WaitForSeconds(fireRate);
+        Shooting = false;
+    }
+
+    public void OnTriggerEnter(Collider obj)
+    {
+        if(obj.CompareTag("Player"))
+        {
+            NRange = true;
+        }
+    }
+
+    public void OnTriggerExit(Collider obj)
+    {
+        if (obj.CompareTag("Player"))
+        {
+            NRange = false;
+        }
+    }
+}
+>>>>>>> Stashed changes
--- SpiritDragon_Prototype_FPS/Assets/Scripts/gameManager.cs
+++ SpiritDragon_Prototype_FPS/Assets/Scripts/gameManager.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -34,3 +35,93 @@ public class gameManager : MonoBehaviour
         }
     }
 }
+=======
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+    public static gameManager instance;
+
+    [Header("----- Player Data -----")]
+    public GameObject player;
+    public playerController playerScript;
+
+    [Header("----- UI -----")]
+    public GameObject activeMenu;
+    public GameObject pauseMenu;
+    public GameObject winMenu;
+    public GameObject loseMenu;
+    public GameObject playerDamageFlashScreen;
+
+    [Header("----- Game Goals -----")]
+    public int enemiesRemaining;
+
+
+    public bool isPaused;
+
+    void Awake()
+    {
+        instance = this;
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerScript = player.GetComponent<playerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetButtonDown("Cancel") && activeMenu == null)
+        {
+            isPaused = !isPaused;
+            activeMenu = pauseMenu;
+            activeMenu.SetActive(isPaused);
+
+            if (isPaused)
+                pause();
+            else
+                unPause();
+        }
+    }
+
+    public void pause()
+    {
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+
+    }
+
+    public void unPause()
+    {
+        Time.timeScale = 1;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        activeMenu.SetActive(false);
+        activeMenu = null;
+    }
+
+    public void updateGameGoal(int amount)
+    {
+        enemiesRemaining += amount;
+
+        if (enemiesRemaining <= 0)
+        {
+            setMenu(winMenu);
+        }
+    }
+
+    public void playerDead()
+    {
+        setMenu(loseMenu);
+    }
+
+    public void setMenu(GameObject menu)
+    {
+        pause();
+        activeMenu = menu;
+        activeMenu.SetActive(true);
+    }
+}
+>>>>>>> Stashed changes
