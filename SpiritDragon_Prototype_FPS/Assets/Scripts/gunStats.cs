1c2733cd6019784c29783d56432d57bc55a71e74
Leonard Stockton <100545279+LeonardStockton@users.noreply.github.com>
2023-02-20 15:07:16 -0500

    small changes in weapons (continued)

---
+++ SpiritDragon_Prototype_FPS/Assets/Scripts/gunStats.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class gunStats : ScriptableObject
+{
+    public float shootRate;
+    public int shootDist;
+    public int shootDamage;
+    public int weaponAmmo;
+    public GameObject weaponSkin;
+    public AudioClip weaponFireAud;
+}
