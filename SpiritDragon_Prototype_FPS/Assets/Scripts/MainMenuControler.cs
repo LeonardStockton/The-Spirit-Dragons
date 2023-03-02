fc4ec5a41b4d7fb4190ef8aa5ad873f003961fa1
Leonard Stockton <100545279+LeonardStockton@users.noreply.github.com>
2023-03-01 20:22:20 -0500

    push of current main menu work

--- SpiritDragon_Prototype_FPS/Assets/Scripts/MainMenuControler.cs
+++ SpiritDragon_Prototype_FPS/Assets/Scripts/MainMenuControler.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
+using TMPro;
 public class MainMenuControler : MonoBehaviour
 {
-    [Header("level to load")]
+    [Header("~~~~~~level to load~~~~~~")]
     public string _newGameLevel;
     private string levelToLoad;
     [SerializeField] private GameObject noSaveFile = null;
+
+    [Header("~~~~~~Audio settings~~~~~")]
+    [SerializeField] private TMP_Text volumeTextValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private float defualtVolume = 1.0f;
+
+    [SerializeField] private GameObject confermationPrompt=null;
 
     public void NewGameDialog()
     {
@@ -34,4 +41,35 @@ public class MainMenuControler : MonoBehaviour
         Application.Quit();
     }
 
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeTextValue.text = volume.ToString("0.0");
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVaolume", AudioListener.volume);
+        //show prompt 
+        StartCoroutine(ConfermationBox());
+    }
+    public IEnumerator ConfermationBox()
+    {
+        confermationPrompt.SetActive(true);
+        yield return new WaitForSeconds(3);
+        confermationPrompt.SetActive(false);
+
+    }
+
+    public void Resetbutton(string menuType)
+    {
+        if (menuType=="Sound")
+        {
+            AudioListener.volume = defualtVolume;
+            volumeSlider.value = defualtVolume;
+            volumeTextValue.text = defualtVolume.ToString("0.0");
+            VolumeApply();
+        }
+
+    }
 }
